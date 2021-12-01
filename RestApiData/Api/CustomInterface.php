084ed2d4131e4633ae7a8b865a180332611eb886
ambabakshay <akshay.pawar@ambab.com>
2021-12-01 15:29:44 +0530

    Order Data fetch using rest api

---
+++ RestApiData/Api/CustomInterface.php
@@ -0,0 +1,11 @@
+<?php
+namespace Ambab\RestApiData\Api;
+interface CustomInterface
+{
+    /**
+    * @api
+    * @param int 
+    * @return string 
+    */
+    public function getOrderId($value);
+}
