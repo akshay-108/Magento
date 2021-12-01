40bcaae5bd2be011e86671c5865b06d50cb46473
Ashwin-ambab <ashwin.gupta@ambab.com>
2021-12-01 12:54:44 +0530

    data get using rest api

--- app/code/Ambab/RestApiData/Model/Api/Custom.php
+++ app/code/Ambab/RestApiData/Model/Api/Custom.php
@@ -1,17 +1,26 @@
 <?php
 namespace Ambab\RestApiData\Model\Api;
 use Psr\Log\LoggerInterface;
+use Magento\Store\Model\StoreManagerInterface;
+use  \Magento\Catalog\Api\ProductRepositoryInterface;
 class Custom
 {
     protected $logger;
     protected $orderRepository;
+    protected $productRepository;
+    protected $_storeManager;
+
     public function __construct(
         LoggerInterface $logger,
-        \Magento\Sales\Api\OrderRepositoryInterface $orderRepository
+        \Magento\Sales\Api\OrderRepositoryInterface $orderRepository,
+        ProductRepositoryInterface $productRepository,
+        StoreManagerInterface $storeManager
     )
     {
         $this->logger = $logger;
         $this->orderRepository = $orderRepository;
+        $this->productRepository = $productRepository;
+        $this->_storeManager = $storeManager;
     }
     /**
      * @inheritdoc
@@ -20,24 +29,44 @@ class Custom
     {
         try {
             $order = $this->orderRepository->get($value);
-            $payment = $order->getPayment()->getData();
+            
+            // get image url
+           
+            // $shippingInfo=[];
             $collection=[];
             foreach ($order->getAllItems() as $item) 
             {
-                $collection['product information']=[
+                $product=$this->productRepository->getByid($item->getProductId());
+                $store = $this->_storeManager->getStore();
+                $imageURL = $store->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA) . 'catalog/product' . $product->getImage();
+
+                // get final price
+                $finalPrice=$item->getOriginalPrice() * $item->getQtyOrdered();
+
+                $collection[]=[
                     'Product Name'=>$item->getName(),
+                    'Image url'=>$imageURL,
+                    'Order Date'=>$item->getCreatedAt(),
                     'Regular Price'=>$item->getOriginalPrice(),
-                    'Image'=>$item->getImageUrl(),
                     'Discount'=>$item->getDiscountAmount(),
                     'Quantity'=>$item->getQtyOrdered(),
                     'Special Price'=>$item->getPrice(),
+                    'Total Order'=>$finalPrice,
                 ];
+
+                $shippingInfo=[
+                    'Shipping Address'=>$order->getShippingAddress()->getData(),
+                    'Shipping Amount'=>$order->getShippingAmount(),
+                    'Shipping Method'=>$order->getShippingMethod(),
+                    'Payment Method'=>$order->getPayment()->getMethod(),
+                ];
+                $response = [$collection,$shippingInfo];
             } 
         } catch (\Exception $e) {
             $response = ['success' => false, 'message' => $e->getMessage()];
             $this->logger->info($e->getMessage());
         }
-        // $returnArray = json_encode($response);
-        return $collection; 
+        return $response;
+        
    }
 }
